6a26787f08f839466863b8dd50a477d9ea21ba97
Migal <migallewis@gmail.com>
2020-08-07 09:52:51 +0200

    Added test for actutor health check and test for swagger bean

--- src/test/java/za/co/valr/valrtest/ValrTestApplicationTests.java
+++ src/test/java/za/co/valr/valrtest/ValrTestApplicationTests.java
@@ -1,13 +1,42 @@
 package za.co.valr.valrtest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ValrTestApplicationTests {
+
+	@Autowired
+	private WebApplicationContext context;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() {
+
+	}
 
 	@Test
 	void contextLoads() {
+			context.containsBean("swaggerApi");
+	}
+
+	@Test
+	public void actuatorIsUp() throws Exception {
+		this.mockMvc.perform(get("/actuator/health")).
+				andExpect(status().isOk())
+				.andExpect(content().contentType("application/vnd.spring-boot.actuator.v3+json"))
+				.andExpect(content().string("{\"status\":\"UP\"}"));
 	}
 
 }

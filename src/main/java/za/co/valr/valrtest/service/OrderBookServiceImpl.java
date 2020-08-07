923ef98c82506ca843b0fbad727c1ff7085b8b0f
Migal <migallewis@gmail.com>
2020-08-07 20:46:25 +0200

    removed temp files

--- src/main/java/za/co/valr/valrtest/service/OrderBookServiceImpl.java
+++ src/main/java/za/co/valr/valrtest/service/OrderBookServiceImpl.java
@@ -1,6 +1,5 @@
 package za.co.valr.valrtest.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.co.valr.valrtest.mapper.OrderBookMapper;
@@ -8,8 +7,6 @@ import za.co.valr.valrtest.model.OrderBookEntity;
 import za.co.valr.valrtest.model.OrderBook;
 import za.co.valr.valrtest.repository.OrderBookRepository;
 
-import java.io.File;
-import java.io.IOException;
 
 @Service("orderBook")
 public class OrderBookServiceImpl implements OrderBookService {
@@ -21,16 +18,6 @@ public class OrderBookServiceImpl implements OrderBookService {
         OrderBookEntity orderBookEntity = orderBookRepository.findByCurrencyPair(currencyPair);
         OrderBook orderBookResponse = orderBookMapper.orderBookEntityToOrderBook(orderBookEntity);
         return orderBookResponse;
-    }
-
-    void saveOrderBook() throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        OrderBookEntity orderBookEntity = objectMapper.readValue(getFile("Orderbook.json"), OrderBookEntity.class);
-        orderBookRepository.save(orderBookEntity);
-    }
-
-    File getFile(String fileName) {
-        return new File("/home/migal/Projects/forHome/valr/valr-test/src/main/resources/data/Orderbook.json");
     }
 
     @Autowired

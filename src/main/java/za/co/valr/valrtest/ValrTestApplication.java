b0fda1d79764009350ae90c6c2f968aa895c1131
Migal <migallewis@gmail.com>
2020-08-07 08:56:54 +0200

    initialized project

---
+++ src/main/java/za/co/valr/valrtest/ValrTestApplication.java
@@ -0,0 +1,13 @@
+package za.co.valr.valrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValrTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValrTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/valr/valrtest/ValrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.valr.valrtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValrTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

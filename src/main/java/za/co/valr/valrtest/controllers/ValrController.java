54d9d5a7970f1bb644b2dd60400f3a622065c378
Migal <migallewis@gmail.com>
2020-08-11 15:15:38 +0200

    exposed trade history

--- src/main/java/za/co/valr/valrtest/controllers/ValrController.java
+++ src/main/java/za/co/valr/valrtest/controllers/ValrController.java
@@ -6,15 +6,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.valr.valrtest.model.OrderBook;
+import za.co.valr.valrtest.model.Trade;
 import za.co.valr.valrtest.service.ValrService;
+
+import java.util.List;
 
 @RestController
 public class ValrController {
     @Autowired
-    private ValrService orderBookService;
+    private ValrService valrService;
 
     @GetMapping("{currencyPair}/orderbook")
     public @ResponseBody OrderBook getOrderBook(@PathVariable String currencyPair) {
-        return this.orderBookService.getOrderBook(currencyPair);
+        return this.valrService.getOrderBook(currencyPair);
+    }
+    @GetMapping("{currencyPair}/tradeHistory")
+    public @ResponseBody List<Trade> getAllTrades(@PathVariable String currencyPair) {
+        return this.valrService.getAllTrades(currencyPair);
     }
 }
--- src/test/java/za/co/valr/valrtest/ValrTestApplicationTests.java
+++ src/test/java/za/co/valr/valrtest/ValrTestApplicationTests.java
@@ -2,19 +2,26 @@ package za.co.valr.valrtest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import za.co.valr.valrtest.model.OrderBook;
 import za.co.valr.valrtest.model.OrderBookEntity;
+import za.co.valr.valrtest.model.Trade;
+import za.co.valr.valrtest.model.TradeEntity;
 import za.co.valr.valrtest.repository.OrderBookRepository;
+import za.co.valr.valrtest.repository.TradeRepository;
 
 import java.io.File;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -29,12 +36,15 @@ class ValrTestApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 	private OrderBookRepository orderBookRepository;
+	private TradeRepository tradeRepository;
 	private ObjectMapper objectMapper;
 
 	@BeforeEach
 	void setUp() {
 		this.objectMapper = new ObjectMapper();
+		this.objectMapper.registerModule(new JavaTimeModule());
 		this.orderBookRepository = (OrderBookRepository) context.getBean("orderBookRepository");
+		this.tradeRepository = (TradeRepository) context.getBean("tradeRepository");
 	}
 
 	@Test
@@ -67,6 +77,20 @@ class ValrTestApplicationTests {
 		this.mockMvc.perform(get("/BTCZAR/orderbook")).
 				andExpect(status().isOk()).
 				andExpect(content().string(objectMapper.writeValueAsString(expResult)));
+	}
+
+	@Test
+	public void getAllTrades() throws Exception {
+		TypeReference<List<TradeEntity>> typeRef
+				= new TypeReference<List<TradeEntity>>() {};
+		List<TradeEntity> tradeEntities = objectMapper.readValue(getFile("TradeHistory.json"), typeRef);
+
+		tradeRepository.saveAll(tradeEntities);
+
+
+		this.mockMvc.perform(get("/BTCZAR/tradeHistory")).
+				andExpect(status().isOk()).
+				andExpect(content().contentType(MediaType.APPLICATION_JSON));
 	}
 
 	File getFile(String fileName) {
